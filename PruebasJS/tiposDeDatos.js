c6b20217a66acc35c238727c3715686c9ec060f2
ValeAngeletti <valeangelett11@gmail.com>
2024-03-14 12:51:43 -0300

    Archivos iniciales

---
+++ PruebasJS/tiposDeDatos.js
@@ -0,0 +1,5 @@
+console.log(typeof "Angeletti");
+console.log(typeof 20);
+console.log(typeof true);
+console.log(typeof null);
+console.log(typeof undefined);
---
+++ PruebasJS/variable.js
@@ -0,0 +1,37 @@
+let nombreA = "Valentina" ;
+let edad = 20 ;
+let trabajo = true ;
+let pajaro ;
+let carpincho = null ;
+
+console.log("Nombre = ", nombre);
+console.log("Edad = ", edad);
+console.log("¿Trabajo? = ", trabajo);
+console.log(" Pajaro = ", pajaro);
+console.log("Carpincho = ", carpincho);
+
+//Ejercicio 6 dia 1
+
+let variableD1 ;
+let variableD2 ;
+let variableD3 ;
+let variableD4 ;
+
+//Ejercicio 7 dia 1
+
+let variable1 = "perro";
+let variable2 = 27;
+let variable3 = " vicente";
+let variable4 = 7; 
+
+//Ejercicio 8 dia 1
+
+let nombre ="Valentina";
+let apellido = "Angeletti";
+let estadoCivil = "Soltera" ;
+let pais = "Argentina";
+let edad2 = 20;
+
+//Ejercicio 9 dia 1
+
+let nombre1 = "Valentina", apellido1 = "Angeletti", estadoCivil1 = "Soltera", pais1 = "Argentina", edad1 = 20;
